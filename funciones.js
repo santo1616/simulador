d7b0e8c7bdec13fa2de32d60168bb9a2334432e3
santo1616 <santo1616@gmail.com>
2026-04-19 23:31:56 -0500

    Calcular el disponible ok

--- funciones.js
+++ funciones.js
@@ -1,1 +1,13 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos, egresos) {
+
+    let disponible = ingresos - egresos
+
+    if (disponible < 0) {
+        return 0;
+
+    }
+    return disponible;
+
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,9 @@
 //AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+
+function calcular (){
+let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+let egresos = parseFloat(document.getElementById("txtEgresos").value);
+let disponible = calcularDisponible(ingresos, egresos);
+
+document.getElementById("spnDisponible").innerText = "USD " + disponible.toFixed(2);
+}
